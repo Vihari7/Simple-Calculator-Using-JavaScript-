f5fadfd000d609ecfa62361de76462bdd44616ac
Vihari Gunarathna <106915486+Vihari7@users.noreply.github.com>
2023-12-14 06:30:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+
+//  FCB -Simple Javascript Program
+// Student ID -6194
+// W.D.Vihari Gunarathna 
+
+function appendValue(value) {
+    document.getElementById('display').value += value;
+  }
+
+  function clearDisplay() {
+    document.getElementById('display').value = '';
+  }
+
+  function backspace() {
+      let display = document.getElementById('display');
+      if (display.value.length > 0) {
+          display.value = display.value.slice(0, -1);
+      }
+  }
+
+  function calculateResult() {
+    try {
+      let inputValue = document.getElementById('display').value;
+
+      // Check if the input contains a percentage sign
+      if (inputValue.includes('%')) {
+        // Remove the percentage sign and calculate the percentage
+        let percentageValue = parseFloat(inputValue) / 100;
+        document.getElementById('display').value = percentageValue;
+      } else {
+        // If no percentage sign, use eval for other calculations
+        document.getElementById('display').value = eval(inputValue);
+      }
+    } catch (error) {
+      document.getElementById('display').value = 'Error';
+    }
+  }
